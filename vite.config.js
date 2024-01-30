be9c05551ef7b134d15a767fa297d1f8c2f4bd01
diaskalana <74653324+diaskalana@users.noreply.github.com>
2024-01-30 20:25:20 +0530

    Update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
     plugins: [
         // input https://www.npmjs.com/package/html-minifier-terser options
         ViteMinifyPlugin({}),
     ],
-    css: {
-        postcss: {
-            plugins: [
-                autoprefixer({}) // add options if needed
-            ],
-        }
-    }
+    base: "/ToolSet/"
 })
